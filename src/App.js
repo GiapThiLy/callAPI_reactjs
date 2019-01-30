a3792c4351a323c750d2ba84b243b0da012ede25
GiapThiLy <you@example.com>
2019-01-30 13:35:03 +0700

    first code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Menu from './components/Menu/Menu';
+import routes from './routes';
+import {Switch,Route,BrowserRouter as Router} from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Menu/>        
+          <div className="container">          
+            <div className="row">           
+              {this.showContentMenus(routes)}
+            </div>          
+          </div>        
+        </div>      
+      </Router>
+
     );
+  }
+  showContentMenus=(routes)=>{
+    var result=null;
+    if(routes.length>0){
+      result=routes.map((route,index)=>{
+        return(
+         <Route
+            key={index}
+            path={route.path}
+            component={route.main}
+            exact={route.exact}
+         />
+        );
+      });
+    }
+    return <Switch>{result}</Switch>;
   }
 }
 
